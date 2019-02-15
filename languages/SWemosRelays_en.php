47c35eb87d43b85f5f4f47ce7ecf99ba1941fa97
tarasfrompir <tarasfrompir@inbox.ru>
2019-02-15 15:18:06 +0200

    Update SWemosRelays_en.php

--- languages/SWemosRelays_en.php
+++ languages/SWemosRelays_en.php
@@ -1,7 +1,6 @@
 <?php
 $dictionary = array(
  'SWemosRelays_MODULE_NAME' => 'Wemos Relay',
- 'SWemosRelays_STRUCTURE_NAME' => 'Switch from Wemos',
 );
 foreach ($dictionary as $k => $v) {
  if (!defined('LANG_' . $k)) {
