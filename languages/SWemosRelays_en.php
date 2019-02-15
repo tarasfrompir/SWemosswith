2d7b02be0c047d8d648cd545359948179f7065df
tarasfrompir <tarasfrompir@inbox.ru>
2019-02-15 14:19:27 +0200

    Update SWemosRelays_en.php

--- languages/SWemosRelays_en.php
+++ languages/SWemosRelays_en.php
@@ -1,1 +1,9 @@
-vcvc
+<?php
+$dictionary = array(
+ 'SMagXXXdevice_MODULE_NAME' => 'Wemos Relay',
+);
+foreach ($dictionary as $k => $v) {
+ if (!defined('LANG_' . $k)) {
+  define('LANG_' . $k, $v);
+ }
+}
