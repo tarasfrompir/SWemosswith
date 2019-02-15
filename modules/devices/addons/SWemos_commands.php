2c7516c7535e71075a7b0c5dccc58e973320d4ab
tarasfrompir <tarasfrompir@inbox.ru>
2019-02-15 15:35:18 +0200

    Update SWemos_commands.php

--- modules/devices/addons/SWemos_commands.php
+++ modules/devices/addons/SWemos_commands.php
@@ -1,3 +1,13 @@
+<?php
+if (defined('DISABLE_SIMPLE_DEVICES') && DISABLE_SIMPLE_DEVICES == 1) return;
+
+/*
+ * array('level' => $level, 'message' => $ph, 'member_id' => $member_id)
+ * $details['BREAK'] = 1 / 0
+ */
+@include_once(ROOT . 'languages/' . $this->name . '_' . SETTINGS_SITE_LANGUAGE . '.php');
+@include_once(ROOT . 'languages/' . $this->name . '_default' . '.php');
+
 if ($device_type == 'wemos_relay') {
  if (preg_match('/' . LANG_DEVICES_PATTERN_TURNON . '/uis', $command)) {
   sayReplySafe(LANG_TURNING_ON . ' ' . $device_title . $add_phrase, 2);
