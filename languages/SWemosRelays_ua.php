ac950db2e4a19c93e2e6a9ec3b5a73df91d1cb30
tarasfrompir <tarasfrompir@inbox.ru>
2019-04-26 22:36:08 +0300

    Update SWemosRelays_ua.php

--- languages/SWemosRelays_ua.php
+++ languages/SWemosRelays_ua.php
@@ -1,6 +1,6 @@
 <?php
 $dictionary = array(
- 'SWemosRelays_MODULE_NAME' => 'Простий Пристрій-Wemos вимикач',
+ 'SWemosRelays_MODULE_NAME' => 'Простий Пристрій - Wemos вимикач',
 );
 foreach ($dictionary as $k => $v) {
  if (!defined('LANG_' . $k)) {
